4f9c7444ce1c5cbd2263e5b959f65c4b47eabb0b
mohsen ataei <mohsenataeiasad@gmail.com>
2020-03-28 18:49:53 +0430

    init project and some packages.

---
+++ projectboard/src/main/java/com/mohsen/projectboard/services/ProjectTaskService.java
@@ -0,0 +1,11 @@
+package com.mohsen.projectboard.services;
+
+import com.mohsen.projectboard.repository.ProjectTaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectTaskService {
+    @Autowired
+    private ProjectTaskRepository projectTaskRepository;
+}
